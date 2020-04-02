32a9df08840e2c1486772bf5612fa5c1c0ca35e6
Administrator <1171969053@qq.com>
2020-04-02 21:44:06 +0800

    update

---
+++ spring-cloud-alibaba-provider/src/main/java/com/learn/springcloud/alibaba/provider/SpringCloudAlibabaProviderApplication.java
@@ -0,0 +1,25 @@
+package com.learn.springcloud.alibaba.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringCloudAlibabaProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAlibabaProviderApplication.class, args);
+    }
+
+    @RestController
+    public class EchoController {
+        @GetMapping(value = "/echo/{message}")
+        public String echo(@PathVariable String message) {
+            return "Hello Nacos Discovery " + message;
+        }
+    }
+}
---
+++ spring-cloud-alibaba-provider/src/test/java/com/learn/springcloud/alibaba/provider/SpringCloudAlibabaProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springcloud.alibaba.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAlibabaProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
