e54f770595c5b89d5e41615c112acccd19c4477d
Administrator <1171969053@qq.com>
2020-04-03 09:19:26 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # spring-cloud-alibaba
+
+jvm优化参数
+---
+-XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=128m -Xms1024m -Xmx1024m -Xmn256m -Xss256k -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC
+---
--- spring-cloud-alibaba-consumer/src/main/java/com/learn/springcloud/alibaba/consumer/Controller/TestController.java
+++ spring-cloud-alibaba-consumer/src/main/java/com/learn/springcloud/alibaba/consumer/Controller/TestController.java
@@ -1,19 +1,30 @@
 package com.learn.springcloud.alibaba.consumer.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@RefreshScope //配置实时生效, 刷新容器里bean的值
 @RestController
 public class TestController {
 
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${user.name}")
+    private String username;
+
     @GetMapping("/hi/{msg}")
     public String sayHi(@PathVariable("msg") String msg){
         return restTemplate.getForObject("http://nacos-provider/hi/" + msg, String.class);
+    }
+
+    @GetMapping("/hi")
+    public String sayHi(){
+        return "username是: " + username;
     }
 }
