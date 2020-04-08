ac1b7a72164c552abc93846f86f213710126e043
Administrator <1171969053@qq.com>
2020-04-08 23:43:33 +0800

    update

---
+++ spring-cloud-alibaba-dubbo/spring-cloud-alibaba-dubbo-provider/dubbo-service/src/test/java/com/learn/dubbo/service/DubboServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.dubbo.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
