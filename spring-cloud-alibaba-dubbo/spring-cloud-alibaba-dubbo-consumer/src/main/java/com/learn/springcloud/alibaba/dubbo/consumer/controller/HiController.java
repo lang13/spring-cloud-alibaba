9852bc31928f26dc45938747f6fd81e9a13d002b
Administrator <1171969053@qq.com>
2020-05-11 17:53:43 +0800

    update

--- spring-cloud-alibaba-dubbo/spring-cloud-alibaba-dubbo-consumer/src/main/java/com/learn/springcloud/alibaba/dubbo/consumer/controller/HiController.java
+++ spring-cloud-alibaba-dubbo/spring-cloud-alibaba-dubbo-consumer/src/main/java/com/learn/springcloud/alibaba/dubbo/consumer/controller/HiController.java
@@ -2,18 +2,29 @@ package com.learn.springcloud.alibaba.dubbo.consumer.controller;
 
 import com.learn.dubbo.api.HiService;
 import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class HiController {
 
     @Reference
     private HiService hiService;
 
+    @Value("${user.name}")
+    private String username;
+
     @GetMapping("/hi/{msg}")
     public String Hi(@PathVariable String msg){
         return hiService.Hi(msg);
+    }
+
+    @GetMapping("/config")
+    public String config(){
+        return "配置里user.name的值是: " + username;
     }
 }
---
+++ spring-cloud-alibaba-dubbo/spring-cloud-alibaba-dubbo-consumer/src/test/java/com/learn/springcloud/alibaba/dubbo/consumer/SpringCloudAlibabaDubboConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springcloud.alibaba.dubbo.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAlibabaDubboConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
